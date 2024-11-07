d1585b37fa7d326e0dbd360ca1e2dc05212e74e2
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-11-07 15:47:23 -0600

    Update view-header.php

--- universal/view-header.php
+++ universal/view-header.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?=$pageTitle?></title>
-    <link rel="icon" type="image/x-icon" href="/img/kky-crest.png">
+    <link rel="icon" type="image/x-icon" href="/universal/img/kky-crest.png">
 
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
@@ -28,7 +28,7 @@
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
       <div class="container-fluid">
         <a class="navbar-brand">
-          <img src="/img/kky-banner.png" width="300px">
+          <img src="/universal/img/kky-banner.png" width="300px">
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
