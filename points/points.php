4683cc71a2f29b89c9e4a0858b7b531561f1315e
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-11-07 14:59:40 -0600

    Create points.php

---
+++ points/points.php
@@ -0,0 +1,17 @@
+<?php
+$pageTitle = "Home";
+$homeClass = "nav-link";
+$brothersClass = "nav-link dropdown-toggle";
+$pointsClass = "nav-link active";
+$attendanceClass = "nav-link";
+$demographicsClass = "nav-link";
+include "universal/view-header.php";
+?>
+
+<h1>Points</h1>
+
+<p>Info here</p>
+
+<?php
+include "universal/view-footer.php";
+?>
