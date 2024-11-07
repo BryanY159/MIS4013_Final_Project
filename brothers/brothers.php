7cba42b28b3e14be0e01e59b504b0e417e66440a
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-11-07 14:58:56 -0600

    Create brothers.php

---
+++ brothers/brothers.php
@@ -0,0 +1,17 @@
+<?php
+$pageTitle = "Brothers";
+$homeClass = "nav-link";
+$brothersClass = "nav-link active dropdown-toggle";
+$pointsClass = "nav-link";
+$attendanceClass = "nav-link";
+$demographicsClass = "nav-link";
+include "universal/view-header.php";
+?>
+
+<h1>Brothers</h1>
+
+<p>Info here</p>
+
+<?php
+include "universal/view-footer.php";
+?>
