7c32bcd8bd95489490f0dc3444983b5801097c44
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-11-07 17:58:11 -0600

    Update view.php

--- brothers/view.php
+++ brothers/view.php
@@ -1,1 +1,37 @@
+<h1>Brothers</h1>
 
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Section</th>
+        <th>Status</th>
+        <th>Membership Class</th>
+        <th>Initiation Year</th>
+        <th>Graduation Year</th>
+        <th>Major(s)</th>
+        <th>Minor(s)</th>
+        <th></th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+        while($brother = $brothers->fetch_assoc()) { ?>
+          <tr>
+            <td><?php echo $brother['FirstName'];?> <?php echo $brother['LastName'];?></td>
+            <td><?php echo $brother['InstrumentName'];?></td>
+            <td><?php echo $brother['Status'];?></td>
+            <td><?php echo $brother['MembershipClass'];?></td>
+            <td><?php echo $brother['InitiationYear'];?></td>
+            <td><?php echo $brother['GraduationYear'];?></td>
+            <td><?php echo $brother['Major'];?></td>
+            <td><?php echo $brother['Minor'];?></td>
+            <td>Edit</td>
+            <td>Delete</td>
+          </tr>
+        <?php } ?>
+    </tbody>
+  </table>
+</div>
