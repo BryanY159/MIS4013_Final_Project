448d0b0f1cdfa24a9655c5fe5be7e2c8630b46a8
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-11-07 15:05:35 -0600

    Update filters.php

--- filters/filters.php
+++ filters/filters.php
@@ -1,11 +1,11 @@
 <?php
-$pageTitle = "Home";
+$pageTitle = "Filters";
 $homeClass = "nav-link";
 $brothersClass = "nav-link active dropdown-toggle";
 $pointsClass = "nav-link";
 $attendanceClass = "nav-link";
 $demographicsClass = "nav-link";
-include "universal/view-header.php";
+include "..universal/view-header.php";
 ?>
 
 <h1>Filters</h1>
@@ -13,5 +13,5 @@ include "universal/view-header.php";
 <p>Info here</p>
 
 <?php
-include "universal/view-footer.php";
+include "..universal/view-footer.php";
 ?>
