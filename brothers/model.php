803d0b0e39fcfe5b17bbc70840f611a70362691d
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-11-07 17:51:05 -0600

    Update model.php

--- brothers/model.php
+++ brothers/model.php
@@ -1,1 +1,17 @@
+<?php
 
+function selectBrothers() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT * FROM Brothers B JOIN Sections S ON B.SectionID = S.SectionID ORDER BY FirstName, LastName;");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+?>
