ad3b8f61449dfb9889fb68d7eb43c6d7934d299d
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-11-07 15:00:03 -0600

    Create attendance.php

---
+++ attendance/attendance.php
@@ -0,0 +1,17 @@
+<?php
+$pageTitle = "Home";
+$homeClass = "nav-link";
+$brothersClass = "nav-link dropdown-toggle";
+$pointsClass = "nav-link";
+$attendanceClass = "nav-link active";
+$demographicsClass = "nav-link";
+include "universal/view-header.php";
+?>
+
+<h1>Attendance</h1>
+
+<p>Info here</p>
+
+<?php
+include "universal/view-footer.php";
+?>
