0bca47e8ad2a98da63a21b1a370a94926b007e6b
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-11-07 15:00:31 -0600

    Create demographics.php

---
+++ demographics/demographics.php
@@ -0,0 +1,17 @@
+<?php
+$pageTitle = "Home";
+$homeClass = "nav-link";
+$brothersClass = "nav-link dropdown-toggle";
+$pointsClass = "nav-link";
+$attendanceClass = "nav-link";
+$demographicsClass = "nav-link active";
+include "universal/view-header.php";
+?>
+
+<h1>Demographics</h1>
+
+<p>Info here</p>
+
+<?php
+include "universal/view-footer.php";
+?>
