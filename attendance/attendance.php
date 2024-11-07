851a33eeb569822bd45ba00ca3adf2006773aadf
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-11-07 15:05:56 -0600

    Update attendance.php

--- attendance/attendance.php
+++ attendance/attendance.php
@@ -1,11 +1,11 @@
 <?php
-$pageTitle = "Home";
+$pageTitle = "Attendance";
 $homeClass = "nav-link";
 $brothersClass = "nav-link dropdown-toggle";
 $pointsClass = "nav-link";
 $attendanceClass = "nav-link active";
 $demographicsClass = "nav-link";
-include "universal/view-header.php";
+include "..universal/view-header.php";
 ?>
 
 <h1>Attendance</h1>
@@ -13,5 +13,5 @@ include "universal/view-header.php";
 <p>Info here</p>
 
 <?php
-include "universal/view-footer.php";
+include "..universal/view-footer.php";
 ?>
