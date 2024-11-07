ac3b37c3bf466526130ba8df28ef130945c45e84
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-11-07 15:20:53 -0600

    Update page.php

--- brothers/page.php
+++ brothers/page.php
@@ -5,7 +5,7 @@ $brothersClass = "nav-link active dropdown-toggle";
 $pointsClass = "nav-link";
 $attendanceClass = "nav-link";
 $demographicsClass = "nav-link";
-include "..universal/view-header.php";
+include "../universal/view-header.php";
 ?>
 
 <h1>Brothers</h1>
@@ -13,5 +13,5 @@ include "..universal/view-header.php";
 <p>Info here</p>
 
 <?php
-include "..universal/view-footer.php";
+include "../universal/view-footer.php";
 ?>
