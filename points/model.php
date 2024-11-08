f7708e32cae67a21a92da72dcbcaa46dac437ddb
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-11-07 19:59:35 -0600

    Update model.php

--- points/model.php
+++ points/model.php
@@ -3,7 +3,7 @@
 function selectBrothers() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT FirstName, LastName, ServicePoints, BrotherhoodPoints FROM Brothers WHERE ServicePoints >= 0;");
+        $stmt = $conn->prepare("SELECT FirstName, LastName, ServicePoints, BrotherhoodPoints FROM Brothers WHERE ServicePoints >= 0 ORDER BY FirstName, LastName;");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
