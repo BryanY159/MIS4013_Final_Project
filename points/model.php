27b7689272daf6d55d5df35781776ba0ac5131e0
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-11-07 19:46:24 -0600

    Update model.php

--- points/model.php
+++ points/model.php
@@ -1,1 +1,17 @@
+<?php
 
+function selectBrothers() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT FirstName, LastName, ServicePoints, BrotherhoodPoints FROM Brothers WHERE ServicePoints >= 0;");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+?>
