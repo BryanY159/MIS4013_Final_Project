a4eb15081899e329336a6f039bfa9954624a9b2a
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-11-08 12:01:13 -0600

    Update view.php

--- points/view.php
+++ points/view.php
@@ -36,8 +36,7 @@
             <td><?php echo $brother['FirstName'];?> <?php echo $brother['LastName'];?></td>
             <td>Temp</td>
             <td>
-              <?php echo $brother['ServicePoints'];?>
-              <p>  </p>
+              <?php echo $brother['ServicePoints']."  ";?>
               <button class="btn btn-primary btn-sm">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
                   <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z"/>
@@ -50,8 +49,7 @@
               </button>
             </td>
             <td>
-              <?php echo $brother['BrotherhoodPoints'];?> 
-              <p>  </p>
+              <?php echo $brother['BrotherhoodPoints']." ";?> 
               <button class="btn btn-warning btn-sm">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
                   <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z"/>
