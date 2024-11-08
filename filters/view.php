c47dbc84f6b51bec6bc58d42e95ca63b97a66455
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-11-07 19:13:40 -0600

    Update view.php

--- filters/view.php
+++ filters/view.php
@@ -8,48 +8,50 @@
         <div class="card-header text-center">
           <h3 class="card-title">Sections</h3>
         </div>
-        <div class="card-body d-flex justify-content-center flex-wrap gap-2">
-          <div class="col-6 mb-2">
-            <button class="btn btn-primary w-100">Flutes</button>
-          </div>
-          <div class="col-6 mb-2">
-            <button class="btn btn-primary w-100">Clarinets</button>
-          </div>
-          <div class="col-6 mb-2">
-            <button class="btn btn-primary w-100">Saxophones</button>
-          </div>
-          <div class="col-6 mb-2">
-            <button class="btn btn-primary w-100">Trumpets</button>
-          </div>
-          <div class="col-6 mb-2">
-            <button class="btn btn-primary w-100">Mellophones</button>
-          </div>
-          <div class="col-6 mb-2">
-            <button class="btn btn-primary w-100">Trombones</button>
-          </div>
-          <div class="col-6 mb-2">
-            <button class="btn btn-primary w-100">Baritones</button>
-          </div>
-          <div class="col-6 mb-2">
-            <button class="btn btn-primary w-100">Sousaphones</button>
-          </div>
-          <div class="col-6 mb-2">
-            <button class="btn btn-primary w-100">Snare</button>
-          </div>
-          <div class="col-6 mb-2">
-            <button class="btn btn-primary w-100">Cymbals</button>
-          </div>
-          <div class="col-6 mb-2">
-            <button class="btn btn-primary w-100">Bass Drum</button>
-          </div>
-          <div class="col-6 mb-2">
-            <button class="btn btn-primary w-100">Tenors</button>
-          </div>
-          <div class="col-6 mb-2">
-            <button class="btn btn-primary w-100">Pit</button>
-          </div>
-          <div class="col-6 mb-2">
-            <button class="btn btn-primary w-100">Guard</button>
+        <div class="card-body">
+          <div class="row">
+            <div class="col-6 mb-2">
+              <button class="btn btn-primary w-100">Flutes</button>
+            </div>
+            <div class="col-6 mb-2">
+              <button class="btn btn-primary w-100">Clarinets</button>
+            </div>
+            <div class="col-6 mb-2">
+              <button class="btn btn-primary w-100">Saxophones</button>
+            </div>
+            <div class="col-6 mb-2">
+              <button class="btn btn-primary w-100">Trumpets</button>
+            </div>
+            <div class="col-6 mb-2">
+              <button class="btn btn-primary w-100">Mellophones</button>
+            </div>
+            <div class="col-6 mb-2">
+              <button class="btn btn-primary w-100">Trombones</button>
+            </div>
+            <div class="col-6 mb-2">
+              <button class="btn btn-primary w-100">Baritones</button>
+            </div>
+            <div class="col-6 mb-2">
+              <button class="btn btn-primary w-100">Sousaphones</button>
+            </div>
+            <div class="col-6 mb-2">
+              <button class="btn btn-primary w-100">Snare</button>
+            </div>
+            <div class="col-6 mb-2">
+              <button class="btn btn-primary w-100">Cymbals</button>
+            </div>
+            <div class="col-6 mb-2">
+              <button class="btn btn-primary w-100">Bass Drum</button>
+            </div>
+            <div class="col-6 mb-2">
+              <button class="btn btn-primary w-100">Tenors</button>
+            </div>
+            <div class="col-6 mb-2">
+              <button class="btn btn-primary w-100">Pit</button>
+            </div>
+            <div class="col-6 mb-2">
+              <button class="btn btn-primary w-100">Guard</button>
+            </div>
           </div>
         </div>
       </div>
@@ -61,18 +63,20 @@
         <div class="card-header text-center">
           <h3 class="card-title">Families</h3>
         </div>
-        <div class="card-body d-flex justify-content-center flex-wrap gap-2">
-          <div class="col-6 mb-2">
-            <button class="btn btn-primary w-100">Woodwinds</button>
-          </div>
-          <div class="col-6 mb-2">
-            <button class="btn btn-primary w-100">High Brass</button>
-          </div>
-          <div class="col-6 mb-2">
-            <button class="btn btn-primary w-100">Low Brass</button>
-          </div>
-          <div class="col-6 mb-2">
-            <button class="btn btn-primary w-100">Percussion</button>
+        <div class="card-body">
+          <div class="row">
+            <div class="col-6 mb-2">
+              <button class="btn btn-primary w-100">Woodwinds</button>
+            </div>
+            <div class="col-6 mb-2">
+              <button class="btn btn-primary w-100">High Brass</button>
+            </div>
+            <div class="col-6 mb-2">
+              <button class="btn btn-primary w-100">Low Brass</button>
+            </div>
+            <div class="col-6 mb-2">
+              <button class="btn btn-primary w-100">Percussion</button>
+            </div>
           </div>
         </div>
       </div>
@@ -84,18 +88,20 @@
         <div class="card-header text-center">
           <h3 class="card-title">Membership Classes</h3>
         </div>
-        <div class="card-body d-flex justify-content-center flex-wrap gap-2">
-          <div class="col-6 mb-2">
-            <button class="btn btn-primary w-100">Zeta Mu</button>
-          </div>
-          <div class="col-6 mb-2">
-            <button class="btn btn-primary w-100">Zeta Nu</button>
-          </div>
-          <div class="col-6 mb-2">
-            <button class="btn btn-primary w-100">Zeta Xi</button>
-          </div>
-          <div class="col-6 mb-2">
-            <button class="btn btn-primary w-100">Zeta Omicron</button>
+        <div class="card-body">
+          <div class="row">
+            <div class="col-6 mb-2">
+              <button class="btn btn-primary w-100">Zeta Mu</button>
+            </div>
+            <div class="col-6 mb-2">
+              <button class="btn btn-primary w-100">Zeta Nu</button>
+            </div>
+            <div class="col-6 mb-2">
+              <button class="btn btn-primary w-100">Zeta Xi</button>
+            </div>
+            <div class="col-6 mb-2">
+              <button class="btn btn-primary w-100">Zeta Omicron</button>
+            </div>
           </div>
         </div>
       </div>
@@ -107,21 +113,23 @@
         <div class="card-header text-center">
           <h3 class="card-title">Majors/Minors</h3>
         </div>
-        <div class="card-body d-flex justify-content-center flex-wrap gap-2">
-          <div class="col-6 mb-2">
-            <button class="btn btn-primary w-100">Music</button>
-          </div>
-          <div class="col-6 mb-2">
-            <button class="btn btn-primary w-100">Engineering</button>
-          </div>
-          <div class="col-6 mb-2">
-            <button class="btn btn-primary w-100">Business</button>
-          </div>
-          <div class="col-6 mb-2">
-            <button class="btn btn-primary w-100">Other</button>
-          </div>
-          <div class="col-6 mb-2">
-            <button class="btn btn-primary w-100">w/ Minors</button>
+        <div class="card-body">
+          <div class="row">
+            <div class="col-6 mb-2">
+              <button class="btn btn-primary w-100">Music</button>
+            </div>
+            <div class="col-6 mb-2">
+              <button class="btn btn-primary w-100">Engineering</button>
+            </div>
+            <div class="col-6 mb-2">
+              <button class="btn btn-primary w-100">Business</button>
+            </div>
+            <div class="col-6 mb-2">
+              <button class="btn btn-primary w-100">Other</button>
+            </div>
+            <div class="col-6 mb-2">
+              <button class="btn btn-primary w-100">w/ Minors</button>
+            </div>
           </div>
         </div>
       </div>
