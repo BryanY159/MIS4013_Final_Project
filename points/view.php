a7fd0b3268eb64c7296134ca8a403381b5bf6282
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-11-07 20:29:20 -0600

    Update view.php

--- points/view.php
+++ points/view.php
@@ -25,13 +25,29 @@
             <td><?php echo $brother['FirstName'];?> <?php echo $brother['LastName'];?></td>
             <td><?php echo $brother['ServicePoints'];?></td>
             <td>
-              <button class="btn btn-primary"><i class="bi bi-plus-circle-fill"></i></button> 
-              <button class="btn btn-primary"><i class="bi bi-dash-circle-fill"></i></button>
+              <button class="btn btn-primary">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
+                  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z"/>
+                </svg>
+              </button> 
+              <button class="btn btn-primary">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash-circle-fill" viewBox="0 0 16 16">
+                  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1z"/>
+                </svg>
+              </button>
             </td>
             <td><?php echo $brother['BrotherhoodPoints'];?></td>
             <td>
-              <button class="btn btn-danger"><i class="bi bi-plus-circle-fill"></i></button> 
-              <button class="btn btn-danger"><i class="bi bi-dash-circle-fill"></i></button>
+              <button class="btn btn-danger">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
+                  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z"/>
+                </svg>
+              </button> 
+              <button class="btn btn-danger">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash-circle-fill" viewBox="0 0 16 16">
+                  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1z"/>
+                </svg>
+              </button>
             </td>
           </tr>
         <?php } ?>
