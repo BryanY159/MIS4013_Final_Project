86da39295f94bafef246e7c9110c09b1a4532fb7
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-11-08 12:57:46 -0600

    Update model.php

--- attendance/model.php
+++ attendance/model.php
@@ -5,24 +5,26 @@ function selectAttendance() {
         $conn = get_db_connection();
         $stmt = $conn->prepare("
         SELECT 
-              B.FirstName,
-              B.LastName,
-              MAX(CASE WHEN A.EventID = 1 THEN A.AttendanceStatus END) AS Event_1_Status,
-              MAX(CASE WHEN A.EventID = 2 THEN A.AttendanceStatus END) AS Event_2_Status,
-              MAX(CASE WHEN A.EventID = 3 THEN A.AttendanceStatus END) AS Event_3_Status,
-              MAX(CASE WHEN A.EventID = 4 THEN A.AttendanceStatus END) AS Event_4_Status,
-              MAX(CASE WHEN A.EventID = 5 THEN A.AttendanceStatus END) AS Event_5_Status,
-              MAX(CASE WHEN A.EventID = 6 THEN A.AttendanceStatus END) AS Event_6_Status,
-              MAX(CASE WHEN A.EventID = 7 THEN A.AttendanceStatus END) AS Event_7_Status,
-              MAX(CASE WHEN A.EventID = 8 THEN A.AttendanceStatus END) AS Event_8_Status,
-              MAX(CASE WHEN A.EventID = 9 THEN A.AttendanceStatus END) AS Event_9_Status,
-              MAX(CASE WHEN A.EventID = 10 THEN A.AttendanceStatus END) AS Event_10_Status
+              FirstName,
+              LastName,
+              MAX(CASE WHEN EventID = 1 THEN AttendanceStatus END) AS Event_1_Status,
+              MAX(CASE WHEN EventID = 2 THEN AttendanceStatus END) AS Event_2_Status,
+              MAX(CASE WHEN EventID = 3 THEN AttendanceStatus END) AS Event_3_Status,
+              MAX(CASE WHEN EventID = 4 THEN AttendanceStatus END) AS Event_4_Status,
+              MAX(CASE WHEN EventID = 5 THEN AttendanceStatus END) AS Event_5_Status,
+              MAX(CASE WHEN EventID = 6 THEN AttendanceStatus END) AS Event_6_Status,
+              MAX(CASE WHEN EventID = 7 THEN AttendanceStatus END) AS Event_7_Status,
+              MAX(CASE WHEN EventID = 8 THEN AttendanceStatus END) AS Event_8_Status,
+              MAX(CASE WHEN EventID = 9 THEN AttendanceStatus END) AS Event_9_Status,
+              MAX(CASE WHEN EventID = 10 THEN AttendanceStatus END) AS Event_10_Status
           FROM 
               Attendance A
           JOIN 
               Brothers B ON A.BrotherID = B.BrotherID
           GROUP BY 
-              B.FirstName, B.LastName;
+              FirstName, LastName
+          ORDER BY
+              FirstName, LastName;
         ");
         $stmt->execute();
         $result = $stmt->get_result();
