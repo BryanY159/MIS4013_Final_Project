04665153d2b4eed6315e0c41bfc0ce4b972b84f8
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-11-07 19:49:21 -0600

    Update view.php

--- points/view.php
+++ points/view.php
@@ -1,1 +1,30 @@
+<div class = "row">
+  <div class = "col">
+    <h1>Points</h1>
+  </div>
+  <div class = "col-auto">
+    Temp
+  </div>
+</div>
 
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Service</th>
+        <th>Brotherhood</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+        while($brother = $brothers->fetch_assoc()) { ?>
+          <tr>
+            <td><?php echo $brother['FirstName'];?> <?php echo $brother['LastName'];?></td>
+            <td><?php echo $brother['ServicePoints'];?></td>
+            <td><?php echo $brother['BrotherhoodPoints'];?></td>
+          </tr>
+        <?php } ?>
+    </tbody>
+  </table>
+</div>
