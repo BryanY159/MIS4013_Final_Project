44096fa7c39ba43a8b3d3ad3c53ce93a4938281a
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-11-10 23:34:32 -0600

    Create dropdown-buttons.php

---
+++ attendance/buttons/dropdown-buttons.php
@@ -0,0 +1,25 @@
+if($eventStatus = "Present") {
+  $buttonClass = btn btn-success dropdown-toggle;
+}
+else if($eventStatus = "Excused") {
+  $buttonClass = btn btn-warning dropdown-toggle;
+}
+else {
+  $buttonClass = btn btn-danger dropdown-toggle;
+}
+?>
+
+<td>
+  <div class="dropdown">
+    <button class="<?php echo $buttonClass;?>" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+      <?php echo $eventStatus;?>
+    </button>
+    <ul class="dropdown-menu">
+      <li><button class="dropdown-item" type="button">Present</button></li>
+      <li><button class="dropdown-item" type="button">Excused</button></li>
+      <li><button class="dropdown-item" type="button">Unexcused</button></li>
+    </ul>
+  </div>
+</td>
+
+<?php
