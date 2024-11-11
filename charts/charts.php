a40deecee75d4adbf80f0dfb09f0ae0d38d25fa2
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-11-11 12:55:18 -0600

    Update and rename demographics.php to charts.php

--- charts/charts.php
+++ charts/charts.php
@@ -2,17 +2,16 @@
 require_once("../universal/db-connection.php");
 require_once("model.php");
 
-$pageTitle = "Demographics";
+$pageTitle = "Charts";
 $homeClass = "nav-link";
 $brothersClass = "nav-link dropdown-toggle";
 $pointsClass = "nav-link";
 $attendanceClass = "nav-link";
-$demographicsClass = "nav-link active";
 
 include "../universal/view-header.php";
 ?>
 
-<h1>Demographics</h1>
+<h1>Charts</h1>
 
 <p>Info here</p>
 
