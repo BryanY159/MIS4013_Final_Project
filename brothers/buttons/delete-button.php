0cce1ad9edfcb268564c3aa6fe570511800d8f8f
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-11-11 15:53:38 -0600

    Update delete-button.php

--- brothers/buttons/delete-button.php
+++ brothers/buttons/delete-button.php
@@ -1,1 +1,5 @@
-<button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#addModal" id="add-button">Delete</button>
+<form method="post" action="">
+  <input type="hidden" name="BID" value="<?php echo $brother['BrotherID'];?>">
+  <input type = "hidden" name = "actionType" value = "Delete">
+  <button type="submit" class="btn btn-danger" id="deleteButtonForID:<?php echo $brother['BrotherID'];?>">Delete</button>
+</form>
