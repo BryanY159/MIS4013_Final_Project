236cbbe0d78048afddebb773a15a987d10a920ec
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-11-11 15:58:43 -0600

    Update delete-button.php

--- brothers/buttons/delete-button.php
+++ brothers/buttons/delete-button.php
@@ -1,5 +1,5 @@
 <form method="post" action="">
   <input type="hidden" name="BID" value="<?php echo $brother['BrotherID'];?>">
   <input type = "hidden" name = "actionType" value = "Delete">
-  <button type="submit" class="btn btn-danger" id="deleteButtonForID:<?php echo $brother['BrotherID'];?>">Delete</button>
+  <button type="submit" class="btn btn-danger" id="deleteButtonForID:<?php echo $brother['BrotherID'];?>" onclick="return confirm('Are you sure?');">Delete</button>
 </form>
