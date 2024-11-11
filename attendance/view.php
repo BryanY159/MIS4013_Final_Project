63ac7933e91bcfe35631c9e40ba9ed4a7a7210a4
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-11-11 10:40:52 -0600

    Update view.php

--- attendance/view.php
+++ attendance/view.php
@@ -30,14 +30,14 @@
             <td>
               <?php
                 if($brother['Unexcused_Absences'] > 4) { ?>
-                  <button class="btn btn-danger"><?php echo $brother['Unexcused_Absences'];?></button>
+                  <button class="btn btn-danger btn-sm"><?php echo $brother['Unexcused_Absences'];?></button>
                 <?php }
                 else { ?>
-                  <button class="btn btn-success"><?php echo $brother['Unexcused_Absences'];?></button>
+                  <button class="btn btn-success btn-sm"><?php echo $brother['Unexcused_Absences'];?></button>
                 <?php }
               ?>
             </td>
-            <td><button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#addModal" id="add-button">Edit</button></td>
+            <td><button type="button" class="btn btn-warning btn-sm" data-bs-toggle="modal" data-bs-target="#addModal" id="add-button">Edit</button></td>
             <?php
               for ($i = 1; $i <= 10; $i++) {
                 $eventStatus = $brother['Event_1_Status'];
