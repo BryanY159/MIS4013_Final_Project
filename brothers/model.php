954ef4f07037dd9fdd02cbaadebf47fe09179b02
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-11-11 13:26:44 -0600

    Update model.php

--- brothers/model.php
+++ brothers/model.php
@@ -14,4 +14,62 @@ function selectBrothers() {
     }
 }
 
+function selectSectionsForInput() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT SectionID, SectionName FROM Sections");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function selectMembershipClassForInput() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT Distinct MembershipClass, InitiationYear FROM Brothers ORDER BY InitiationYear");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function selectStatusForInput() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT Distinct Status FROM Brothers");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function addBrother($FN, $LN, $MC, $IY, $GY, $Major, $Major2, $Minor, $Minor2, $Status, $SID, $SP, $BP) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO 
+        Brothers (FirstName, LastName, MembershipClass, InitiationYear, GraduationYear, Major, Major2, Minor, Minor2, Status, SectionID, ServicePoints, BrotherhoodPoints) 
+		Values   (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+        $stmt->bind_param("sssiisssssiii", $FN, $LN, $MC, $IY, $GY, $Major, $Major2, $Minor, $Minor2, $Status, $SID, $SP, $BP);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
 ?>
