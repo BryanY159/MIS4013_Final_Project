27b149029493d778f816893eb76ebbfc157d7c4b
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-11-11 18:01:58 -0600

    Update brothers.php

--- brothers/brothers.php
+++ brothers/brothers.php
@@ -16,11 +16,22 @@ if (isset($_POST['actionType'])) {
       list($MC, $IY) = explode('|', $_POST['MC-IY']);
     
       if (addBrother($_POST['FN'], $_POST['LN'], $MC, $IY, $_POST['GY'], 
-                     $_POST['Major'], $Major2, $Minor, $Minor2, 
-                     $_POST['Status'], $_POST['SID'], $_POST['SP'], $_POST['BP'])) {
+                     $_POST['Major'], $Major2, $Minor, $Minor2, $_POST['Status'], 
+                     $_POST['SID'], $_POST['SP'], $_POST['BP'])) {
         echo '<div class="alert alert-success" role="alert"> Brother Added Successfully </div>';
       } else {
         echo '<div class="alert alert-danger" role="alert"> Error: Brother Not Added </div>';
+      }
+      break;
+    case "Edit":
+      list($MC, $IY) = explode('|', $_POST['MC-IY']);
+    
+      if (editBrother($_POST['FN'], $_POST['LN'], $MC, $IY, $_POST['GY'], 
+                     $_POST['Major'], $Major2, $Minor, $Minor2, $_POST['Status'], 
+                      $_POST['SID'], $_POST['SP'], $_POST['BP'], $_POST['BID'])) {
+        echo '<div class="alert alert-success" role="alert"> Brother Edited Successfully </div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert"> Error: Brother Not Edited </div>';
       }
       break;
     case "Delete":
