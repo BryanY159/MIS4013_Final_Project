736fd9d7f5466c6ee77c0b0805e32fdf661f3d26
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-11-14 18:05:40 -0600

    Update view.php

--- attendance/view.php
+++ attendance/view.php
@@ -38,34 +38,7 @@
               ?>
             </td>
             <td><button class="btn btn-warning btn-sm" style="width: 45px;">Edit</button></td>
-            <?php
-              for ($i = 1; $i <= 10; $i++) {
-                $eventStatus = $brother['Event_1_Status'];
-
-              if($brother["Event_{$i}_Status"] == "Present") {
-                $buttonClass = "btn btn-success btn-sm dropdown-toggle";
-              }
-              else if($brother["Event_{$i}_Status"] == "Excused") {
-                $buttonClass = "btn btn-secondary btn-sm dropdown-toggle";
-              }
-              else {
-                $buttonClass = "btn btn-danger btn-sm dropdown-toggle";
-              }
-            ?>
-
-            <td>
-              <div class="dropdown">
-                <button class="<?php echo $buttonClass;?>" type="button" data-bs-toggle="dropdown" aria-expanded="false" style="width: 100px;">
-                  <?php echo $brother["Event_{$i}_Status"];?>
-                </button>
-                <ul class="dropdown-menu">
-                  <li><button class="dropdown-item" type="button">Present</button></li>
-                  <li><button class="dropdown-item" type="button">Excused</button></li>
-                  <li><button class="dropdown-item" type="button">Unexcused</button></li>
-                </ul>
-              </div>
-            </td>
-            <?php } ?>
+            <?php include dropdown-buttons.php; ?>
           </tr>
         <?php } ?>
     </tbody>
