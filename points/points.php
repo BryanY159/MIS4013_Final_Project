e1563acf1ea79e3e83627e29ed76836d9a55b5bf
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-11-15 19:23:02 -0600

    Update points.php

--- points/points.php
+++ points/points.php
@@ -10,6 +10,18 @@ $attendanceClass = "nav-link";
 
 include "../universal/view-header.php";
 
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Edit":
+      if (editBrother($_POST['BID'], $_POST['SP'], $_POST['BP'])) {
+        echo '<div class="alert alert-success" role="alert"> Points Edited Successfully </div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert"> Error: Points Not Edited </div>';
+      }
+      break;
+  }
+}
+
 $brothers = selectBrothers();
 
 include "view.php";
