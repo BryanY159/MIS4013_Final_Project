58010220c251e2c2f7398a27961e96ef71a7d41a
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-11-16 12:10:39 -0600

    Update minors-chart.php

--- charts/chart-files/minors-chart.php
+++ charts/chart-files/minors-chart.php
@@ -1,4 +1,4 @@
-<div id="minorsChart" style="width: 100%; height: 100%;"></div>
+<div id="minorsChart" style="width: 100%; height: 85%;"></div>
 
 <script>
   document.addEventListener("DOMContentLoaded", function() {
@@ -40,7 +40,7 @@
         type: 'value'
       },
       series: [{
-        name: 'Data',
+        name: 'Brother Count',
         type: 'bar',
         data: [
           <?php
