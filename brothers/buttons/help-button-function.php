dabe0b9a98d2040de171b5c19653e131e6d5f92c
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-11-16 14:45:29 -0600

    Update help-button-function.php

--- brothers/buttons/help-button-function.php
+++ brothers/buttons/help-button-function.php
@@ -24,7 +24,7 @@
         position: 'left'
       },
       {
-        intro: "You can edit Point information here, but it can only be viewed outside the edit window on the Points page. Attendance can only be viewed and edited on the Attendance page.",
+        intro: "You can edit point information here, but it can only be viewed outside the edit window on the Points page. Attendance can only be viewed and edited on the Attendance page.",
         title: "Additional Information",
       }
     ];
