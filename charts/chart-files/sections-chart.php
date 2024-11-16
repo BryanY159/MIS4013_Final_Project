2a6e131f90f06ae3b337de45c506b181e3e96140
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-11-16 11:00:48 -0600

    Update sections-chart.php

--- charts/chart-files/sections-chart.php
+++ charts/chart-files/sections-chart.php
@@ -1,1 +1,28 @@
-Include Works
+<div id="myChart" style="width: 600px; height: 400px;"></div>
+
+<script>
+  document.addEventListener("DOMContentLoaded", function() {
+    const chartDom = document.getElementById('myChart');
+    const myChart = echarts.init(chartDom);
+    
+    const option = {
+      title: {
+        text: 'Sample Bar Chart'
+      },
+      tooltip: {},
+      xAxis: {
+        type: 'category',
+        data: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [{
+        name: 'Data',
+        type: 'bar',
+        data: [65, 59, 80, 81, 56, 55, 40]
+      }]
+    };
+    myChart.setOption(option);
+  });
+</script>
