759262de2ead06ab6e86d07f0afd3322e2f5b999
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-11-16 11:30:40 -0600

    Update sections-chart.php

--- charts/chart-files/sections-chart.php
+++ charts/chart-files/sections-chart.php
@@ -1,4 +1,4 @@
-<div id="sectionsChart" style="width: 100%; height: 400px;"></div>
+<div id="sectionsChart" style="width: 100%; height: 100%;"></div>
 
 <script>
   document.addEventListener("DOMContentLoaded", function() {
