8b69522e4db7006fd36e3ce5c448fc29a80dbfc1
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-11-16 15:16:11 -0600

    Update delete-button.php

--- brothers/buttons/delete-button.php
+++ brothers/buttons/delete-button.php
@@ -1,10 +1,10 @@
-<?php $delete-data-step = "";
+<?php $delete_data_step = "";
 if($count == 1) { ?>
-  $delete-data-step = "data-step='3'";
+  $delete_data_step = "data-step='3'";
 <?php } ?>
 
 <form method="post" action="">
     <input type="hidden" name="BID" value="<?php echo $brother['BrotherID'];?>">
     <input type = "hidden" name = "actionType" value = "Delete">
-    <button type="submit" class="btn btn-danger" <?php echo $delete-data-step; ?> id="deleteButtonForID:<?php echo $brother['BrotherID'];?>">Delete</button>
+    <button type="submit" class="btn btn-danger" <?php echo $delete_data_step; ?> id="deleteButtonForID:<?php echo $brother['BrotherID'];?>">Delete</button>
 </form>
