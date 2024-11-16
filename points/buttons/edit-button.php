d8398dd7514a4bb8ca77943eb50f4765b7d9aaf8
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-11-15 19:17:19 -0600

    Update edit-button.php

--- points/buttons/edit-button.php
+++ points/buttons/edit-button.php
@@ -1,20 +1,20 @@
 <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#editPointsModalForID:<?php echo $brother['BrotherID'];?>" id="editButtonForID<?php echo $brother['BrotherID'];?>">Edit</button>
 
-<div class="modal fade" id="editPointsModalForID:<?php echo $brother['BrotherID'];?>" tabindex="-1" aria-labelledby="addBrotherModalLabel" aria-hidden="true">
+<div class="modal fade" id="editPointsModalForID:<?php echo $brother['BrotherID'];?>" tabindex="-1" aria-labelledby="editPointsModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
         <h1 class="modal-title fs-5" id="editPointsModalLabelForID:<?php echo $brother['BrotherID'];?>">Edit Points</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
-      <div class="modal-body">
+      <div class="modal-body text-start">
         <form method="post" action="">
           <div class="mb-3">
             <label for="SPForID:<?php echo $brother['BrotherID'];?>" class="form-label">Service Points</label>
             <input type="text" class="form-control" id="SPForID:<?php echo $brother['BrotherID'];?>" name="SP" value="<?php echo $brother['ServicePoints'];?>">
           </div>
           <div class="mb-3">
-            <label for="BPForID:<?php echo $brother['BrotherID'];?>" class="form-label">Service Points</label>
+            <label for="BPForID:<?php echo $brother['BrotherID'];?>" class="form-label">Brotherhood Points</label>
             <input type="text" class="form-control" id="BPForID:<?php echo $brother['BrotherID'];?>" name="BP" value="<?php echo $brother['BrotherhoodPoints'];?>">
           </div>
           <input type="hidden" name="BID" value="<?php echo $brother['BrotherID']; ?>">
