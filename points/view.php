eba2b28ce3142769c3b4d7fd19e14f2c232984d3
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-11-15 18:36:55 -0600

    Update view.php

--- points/view.php
+++ points/view.php
@@ -15,6 +15,7 @@
         <th>Completed?</th>
         <th>Service Points</th>
         <th>Brotherhood Points</th>
+        <th></th>
       </tr>
     </thead>
     <tbody>
@@ -39,31 +40,26 @@
               <?php } ?>
             </td>
             <td>
-              <?php echo $brother['ServicePoints'];?>
-              <button class="btn btn-primary btn-sm">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
-                  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z"/>
-                </svg>
-              </button> 
-              <button class="btn btn-primary btn-sm">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash-circle-fill" viewBox="0 0 16 16">
-                  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1z"/>
-                </svg>
-              </button>
+              <?php
+                if($brother['ServicePoints'] >= 5) { ?>
+                  <button class="btn btn-success" style="width: 45px;"><?php echo $brother['ServicePoints'];?></button>
+                <?php }
+                else { ?>
+                  <button class="btn btn-secondary" style="width: 45px;"><?php echo $brother['ServicePoints'];?></button>
+                <?php }
+              ?>
             </td>
             <td>
-              <?php echo $brother['BrotherhoodPoints'];?> 
-              <button class="btn btn-warning btn-sm">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
-                  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z"/>
-                </svg>
-              </button> 
-              <button class="btn btn-warning btn-sm">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash-circle-fill" viewBox="0 0 16 16">
-                  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1z"/>
-                </svg>
-              </button>
+              <?php
+                if($brother['BrotherhoodPoints'] >= 3) { ?>
+                  <button class="btn btn-success" style="width: 45px;"><?php echo $brother['BrotherhoodPoints'];?></button>
+                <?php }
+                else { ?>
+                  <button class="btn btn-secondary" style="width: 45px;"><?php echo $brother['BrotherhoodPoints'];?></button>
+                <?php }
+              ?>
             </td>
+            <td>Edit</td>
           </tr>
         <?php } ?>
     </tbody>
