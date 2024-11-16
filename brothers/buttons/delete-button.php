0730425c58faafa25881b215b9445414924f7454
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-11-16 15:12:57 -0600

    Update delete-button.php

--- brothers/buttons/delete-button.php
+++ brothers/buttons/delete-button.php
@@ -1,14 +1,10 @@
-<?php if($count == 1) { ?>
-  <form method="post" action="">
-    <input type="hidden" name="BID" value="<?php echo $brother['BrotherID'];?>">
-    <input type = "hidden" name = "actionType" value = "Delete">
-    <button type="submit" class="btn btn-danger" data-step="3" id="deleteButtonForID:<?php echo $brother['BrotherID'];?>">Delete</button>
-  </form>
-<?php }
-else { ?>
-  <form method="post" action="">
-    <input type="hidden" name="BID" value="<?php echo $brother['BrotherID'];?>">
-    <input type = "hidden" name = "actionType" value = "Delete">
-    <button type="submit" class="btn btn-danger" id="deleteButtonForID:<?php echo $brother['BrotherID'];?>">Delete</button>
-  </form>
+<?php $delete-data-step = "";
+if($count == 1) { ?>
+  $delete-data-step = "data-step='3'";
 <?php } ?>
+
+<form method="post" action="">
+    <input type="hidden" name="BID" value="<?php echo $brother['BrotherID'];?>">
+    <input type = "hidden" name = "actionType" value = "Delete">
+    <button type="submit" class="btn btn-danger" <?php echo $delete-data-step; ?> id="deleteButtonForID:<?php echo $brother['BrotherID'];?>">Delete</button>
+</form>
