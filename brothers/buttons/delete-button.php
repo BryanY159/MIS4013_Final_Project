d74fd40fae15453cb092a2d20eea942f103ed3a5
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-11-16 14:38:55 -0600

    Update delete-button.php

--- brothers/buttons/delete-button.php
+++ brothers/buttons/delete-button.php
@@ -1,5 +1,14 @@
-<form method="post" action="">
-  <input type="hidden" name="BID" value="<?php echo $brother['BrotherID'];?>">
-  <input type = "hidden" name = "actionType" value = "Delete">
-  <button type="submit" class="btn btn-danger" id="deleteButtonForID:<?php echo $brother['BrotherID'];?>">Delete</button>
-</form>
+<?php if($count == 1) { ?>
+  <form method="post" action="">
+    <input type="hidden" name="BID" value="<?php echo $brother['BrotherID'];?>">
+    <input type = "hidden" name = "actionType" value = "Delete">
+    <button type="submit" class="btn btn-danger" data-step="3" id="deleteButtonForID:<?php echo $brother['BrotherID'];?>">Delete</button>
+  </form>
+<?php }
+else { ?>
+  <form method="post" action="">
+    <input type="hidden" name="BID" value="<?php echo $brother['BrotherID'];?>">
+    <input type = "hidden" name = "actionType" value = "Delete">
+    <button type="submit" class="btn btn-danger" id="deleteButtonForID:<?php echo $brother['BrotherID'];?>">Delete</button>
+  </form>
+<?php } ?>
