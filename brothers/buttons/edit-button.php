fc534177c5736cbad7903fd591cb524987515506
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-11-15 19:15:58 -0600

    Update edit-button.php

--- brothers/buttons/edit-button.php
+++ brothers/buttons/edit-button.php
@@ -1,6 +1,6 @@
 <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#editBrotherModalForID:<?php echo $brother['BrotherID'];?>" id="editButtonForID<?php echo $brother['BrotherID'];?>">Edit</button>
 
-<div class="modal fade" id="editBrotherModalForID:<?php echo $brother['BrotherID'];?>" tabindex="-1" aria-labelledby="addBrotherModalLabel" aria-hidden="true">
+<div class="modal fade" id="editBrotherModalForID:<?php echo $brother['BrotherID'];?>" tabindex="-1" aria-labelledby="editBrotherModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
