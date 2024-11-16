1228d58c7893ac20eb13595738abfc54a82abf5e
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-11-16 13:43:34 -0600

    Update toastify.php

--- universal/toastify.php
+++ universal/toastify.php
@@ -1,38 +1,36 @@
-<?php if (isset($_POST['actionType'])) { ?>
-  <script>
-    const toastMessage = "<?php echo $toastMessage; ?>";
-    const toastType = "<?php echo $toastType; ?>";
-
-    let backgroundColor;
-
-    switch (toastType) {
-      case "add":
-        backgroundColor = "green";
-        break;
-      case "edit":
-        backgroundColor = "yellow";
-        break;
-      case "delete":
-        backgroundColor = "red";
-        break;
-      case "error":
-        backgroundColor = "blue";
-        break;
-      default:
-        backgroundColor = "gray";
-    }
-    
-    if (toastMessage) {
-      Toastify({
-        text: toastMessage,
-        duration: 3000,
-        gravity: "top",
-        position: "right",
-        style: {
-          background: backgroundColor,
-          color: "black"
-        }
-      }).showToast();
-    }
-  </script>
-<?php } ?>
+<script>
+  const toastMessage = "<?php echo $toastMessage; ?>";
+  const toastType = "<?php echo $toastType; ?>";
+  
+  let backgroundColor;
+  
+  switch (toastType) {
+    case "add":
+      backgroundColor = "green";
+      break;
+    case "edit":
+      backgroundColor = "yellow";
+      break;
+    case "delete":
+      backgroundColor = "red";
+      break;
+    case "error":
+      backgroundColor = "blue";
+      break;
+    default:
+      backgroundColor = "gray";
+  }
+  
+  if (toastMessage) {
+    Toastify({
+      text: toastMessage,
+      duration: 3000,
+      gravity: "top",
+      position: "right",
+      style: {
+        background: backgroundColor,
+        color: "black"
+      }
+    }).showToast();
+  }
+</script>
