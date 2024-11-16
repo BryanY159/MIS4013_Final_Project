8e8b269a825182324704fc5ce6b9e726f56daab7
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-11-16 10:31:46 -0600

    Update view.php

--- charts/view.php
+++ charts/view.php
@@ -6,3 +6,12 @@
     <button class="btn btn-info">Need help?</button>
   </div>
 </div>
+
+<div class="chart-grid">
+  <div class="chart-container" id="chart1">Chart 1</div>
+  <div class="chart-container" id="chart2">Chart 2</div>
+  <div class="chart-container" id="chart3">Chart 3</div>
+  <div class="chart-container" id="chart4">Chart 4</div>
+  <div class="chart-container" id="chart5">Chart 5</div>
+  <div class="chart-container" id="chart6">Chart 6</div>
+</div>
