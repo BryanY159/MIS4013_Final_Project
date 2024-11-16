bd609a921fbf0d38511914b23a1c2018579e566e
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-11-16 10:42:38 -0600

    Update view.php

--- charts/view.php
+++ charts/view.php
@@ -1,17 +1,31 @@
-<div class = "row">
-  <div class = "col">
-    <h1>Charts</h1>
+<div style="margin: 20px;">
+  <div class="row">
+    <div class="col">
+      <h1>Charts</h1>
+    </div>
+    <div class="col-auto">
+      <button class="btn btn-info">Need help?</button>
+    </div>
   </div>
-  <div class = "col-auto">
-    <button class="btn btn-info">Need help?</button>
-  </div>
-</div>
 
-<div class="chart-grid">
-  <div class="chart-container" id="chart1">Chart 1</div>
-  <div class="chart-container" id="chart2">Chart 2</div>
-  <div class="chart-container" id="chart3">Chart 3</div>
-  <div class="chart-container" id="chart4">Chart 4</div>
-  <div class="chart-container" id="chart5">Chart 5</div>
-  <div class="chart-container" id="chart6">Chart 6</div>
+  <div class="row row-cols-2 g-4">
+    <div class="col">
+      <div class="p-3 border rounded bg-white text-center">Chart 1</div>
+    </div>
+    <div class="col">
+      <div class="p-3 border rounded bg-white text-center">Chart 2</div>
+    </div>
+    <div class="col">
+      <div class="p-3 border rounded bg-white text-center">Chart 3</div>
+    </div>
+    <div class="col">
+      <div class="p-3 border rounded bg-white text-center">Chart 4</div>
+    </div>
+    <div class="col">
+      <div class="p-3 border rounded bg-white text-center">Chart 5</div>
+    </div>
+    <div class="col">
+      <div class="p-3 border rounded bg-white text-center">Chart 6</div>
+    </div>
+  </div>
 </div>
