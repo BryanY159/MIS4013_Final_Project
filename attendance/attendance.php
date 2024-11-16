d1c34007e358be98a4cb9583935ff0c626c03d8c
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-11-16 13:20:56 -0600

    Update attendance.php

--- attendance/attendance.php
+++ attendance/attendance.php
@@ -39,7 +39,32 @@ $events = selectEvents();
 
 include "view.php";
 
-?><script src="../universal/toastify-script.js"></script><?php
+?>//<script src="../universal/toastify-script.js"></script>
+
+<script>
+  const toastMessage = "<?php echo $toastMessage; ?>";
+  const toastType = "<?php echo $toastType; ?>";
+  const actionType = "<?php echo $actionType; ?>";
+
+  if (toastMessage) {
+    let backgroundColor = actionType === "Delete" ? "red" :
+                          actionType === "Add" ? "green" : 
+                          actionType === "Edit" ? "yellow" : "blue";
+
+    Toastify({
+      text: toastMessage,
+      duration: 3000,
+      gravity: "top",
+      position: "right",
+      style: {
+        background: backgroundColor,
+        color: "black" // Optional: Set text color for better visibility on yellow
+      }
+    }).showToast();
+  }
+</script>
+
+<?php
 
 include "../universal/view-footer.php";
 ?>
