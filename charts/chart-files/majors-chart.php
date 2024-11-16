949724c89f85419c4d75e41edcaaf024a9868370
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-11-16 11:35:01 -0600

    Update majors-chart.php

--- charts/chart-files/majors-chart.php
+++ charts/chart-files/majors-chart.php
@@ -1,4 +1,4 @@
-<div id="majorsChart" style="width: 100%; height: 100%;"></div>
+<div id="majorsChart" style="width: 100%; height: 400px;"></div>
 
 <script>
   document.addEventListener("DOMContentLoaded", function() {
@@ -7,20 +7,46 @@
     
     const option = {
       title: {
-        text: 'Sample Bar Chart'
+        text: 'Monthly Data by Major',
+        subtext: 'Sample Data for Majors',
+        left: 'center',
+        textStyle: {
+          fontSize: 16,
+          fontWeight: 'bold'
+        }
       },
-      tooltip: {},
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {            
+          type: 'shadow'        
+        }
+      },
       xAxis: {
         type: 'category',
-        data: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul']
+        data: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
+        axisLabel: {
+          interval: 0,  // Show all labels
+          rotate: 45    // Rotate labels if needed
+        }
       },
       yAxis: {
-        type: 'value'
+        type: 'value',
+        axisLabel: {
+          formatter: '{value} units'  // Add units to the y-axis values
+        }
       },
       series: [{
         name: 'Data',
         type: 'bar',
-        data: [65, 59, 80, 81, 56, 55, 40]
+        data: [65, 59, 80, 81, 56, 55, 40],
+        itemStyle: {
+          color: '#4b9cd3'  // Customize bar color
+        },
+        label: {
+          show: true,
+          position: 'top',  // Display labels on top of bars
+          formatter: '{c}'  // Show the value
+        }
       }]
     };
     majorsChart.setOption(option);
