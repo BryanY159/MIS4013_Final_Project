ee1fe4ee9cbc1daa9f74940e6c719c30c16e601e
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-11-16 12:04:48 -0600

    Create minors-chart.php

---
+++ charts/chart-files/minors-chart.php
@@ -0,0 +1,60 @@
+<div id="minorsChart" style="width: 100%; height: 100%;"></div>
+
+<script>
+  document.addEventListener("DOMContentLoaded", function() {
+    const chartDom = document.getElementById('minorsChart');
+    const minorsChart = echarts.init(chartDom);
+    
+    const option = {
+      title: {
+        text: 'Brothers by Minor',
+        subtext: 'Includes Minor and Minor2',
+        left: 'center',
+        textStyle: {
+          fontSize: 16,
+          fontWeight: 'bold'
+        }
+      },
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {            
+          type: 'shadow'        
+        }
+      },
+      xAxis: {
+        type: 'category',
+        data: [
+          <?php
+            $minors = selectMinors();
+            while($minor = $minors->fetch_assoc()) {
+              echo "'".$minor['Minor']."', ";
+            } 
+          ?>
+        ],
+        axisLabel: {
+          interval: 0,
+          rotate: 45
+        }
+      },
+     yAxis: {
+        type: 'value'
+      },
+      series: [{
+        name: 'Data',
+        type: 'bar',
+        data: [
+          <?php
+            $minors = selectMinors();
+            while($minor = $minors->fetch_assoc()) {
+              echo $minor['Count'].", ";
+            } 
+          ?>
+        ],
+        itemStyle: {
+          color: '# ffc61e'
+        },
+      }]
+    };
+    minorsChart.setOption(option);
+  });
+</script>
