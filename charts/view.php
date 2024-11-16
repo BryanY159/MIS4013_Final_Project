dfd113594e387f86e07313142dd14f923705fe7f
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-11-16 10:48:29 -0600

    Update view.php

--- charts/view.php
+++ charts/view.php
@@ -8,24 +8,24 @@
     </div>
   </div>
 
-  <div class="row row-cols-2 g-4">
-    <div class="col">
-      <div class="p-3 border rounded bg-white text-center">Chart 1</div>
+  <div class="row g-4">
+    <div class="col-md-6">
+      <div class="p-3 border rounded bg-white text-center" style="height: 300px;">Chart 1</div>
     </div>
-    <div class="col">
-      <div class="p-3 border rounded bg-white text-center">Chart 2</div>
+    <div class="col-md-6">
+      <div class="p-3 border rounded bg-white text-center" style="height: 300px;">Chart 2</div>
     </div>
-    <div class="col">
-      <div class="p-3 border rounded bg-white text-center">Chart 3</div>
+    <div class="col-md-6">
+      <div class="p-3 border rounded bg-white text-center" style="height: 300px;">Chart 3</div>
     </div>
-    <div class="col">
-      <div class="p-3 border rounded bg-white text-center">Chart 4</div>
+    <div class="col-md-6">
+      <div class="p-3 border rounded bg-white text-center" style="height: 300px;">Chart 4</div>
     </div>
-    <div class="col">
-      <div class="p-3 border rounded bg-white text-center">Chart 5</div>
+    <div class="col-md-6">
+      <div class="p-3 border rounded bg-white text-center" style="height: 300px;">Chart 5</div>
     </div>
-    <div class="col">
-      <div class="p-3 border rounded bg-white text-center">Chart 6</div>
+    <div class="col-md-6">
+      <div class="p-3 border rounded bg-white text-center" style="height: 300px;">Chart 6</div>
     </div>
   </div>
 </div>
