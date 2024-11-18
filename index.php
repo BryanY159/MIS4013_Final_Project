9888773ab81a0786aa993845d99714614ed588d6
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-11-18 13:05:09 -0600

    Update index.php

--- index.php
+++ index.php
@@ -15,36 +15,43 @@ include "universal/view-header.php";
     <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
   </div>
 </div>
-<div class="card">
-  <h5 class="card-header">Brothers - Full Roster</h5>
-  <div class="card-body">
-    <h5 class="card-title">Special title treatment</h5>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="brothers/brothers.php" class="btn btn-primary">Full Roster</a>
+
+<div class = "row">
+  <div class = "col">
+    <div class="card">
+      <h5 class="card-header">Brothers - Full Roster</h5>
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="brothers/brothers.php" class="btn btn-primary">Full Roster</a>
+      </div>
+    </div>
+    <div class="card">
+      <h5 class="card-header">Points</h5>
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="points/points.php" class="btn btn-primary">Points</a>
+      </div>
+    </div>
   </div>
-</div>
-<div class="card">
-  <h5 class="card-header">Brothers - Charts</h5>
-  <div class="card-body">
-    <h5 class="card-title">Special title treatment</h5>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="charts/charts.php" class="btn btn-primary">Charts</a>
-  </div>
-</div>
-<div class="card">
-  <h5 class="card-header">Points</h5>
-  <div class="card-body">
-    <h5 class="card-title">Special title treatment</h5>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="points/points.php" class="btn btn-primary">Points</a>
-  </div>
-</div>
-<div class="card">
-  <h5 class="card-header">Attendance</h5>
-  <div class="card-body">
-    <h5 class="card-title">Special title treatment</h5>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="attendance/attendance.php" class="btn btn-primary">Attendance</a>
+  <div class = "col-auto">
+    <div class="card">
+      <h5 class="card-header">Brothers - Charts</h5>
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="charts/charts.php" class="btn btn-primary">Charts</a>
+      </div>
+    </div>
+    <div class="card">
+      <h5 class="card-header">Attendance</h5>
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="attendance/attendance.php" class="btn btn-primary">Attendance</a>
+      </div>
+    </div>
   </div>
 </div>
 
