78456d22c2f7a47f1fa382739f9f92a70d47117e
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-11-18 13:00:53 -0600

    Update index.php

--- index.php
+++ index.php
@@ -10,6 +10,12 @@ include "universal/view-header.php";
 <h1>Home</h1>
 
 <div class="card">
+  <h5 class="card-header">Welcome!</h5>
+  <div class="card-body">
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+  </div>
+</div>
+<div class="card">
   <h5 class="card-header">Featured</h5>
   <div class="card-body">
     <h5 class="card-title">Special title treatment</h5>
