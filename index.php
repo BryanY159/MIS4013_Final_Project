12dd9c2eb42a48ed38f6f4e664206cbfa1ff6bfc
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-11-18 13:11:58 -0600

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ include "universal/view-header.php";
 <div class="card">
   <h5 class="card-header">Welcome!</h5>
   <div class="card-body">
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <p class="card-text">This website contains all KKΨ-relevant information for our chapter. Please check out what pages we have below, or click around the NavBar.</p>
   </div>
 </div>
 
@@ -21,7 +21,6 @@ include "universal/view-header.php";
     <div class="card">
       <h5 class="card-header">Brothers - Full Roster</h5>
       <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
         <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
         <a href="brothers/brothers.php" class="btn btn-primary">Full Roster</a>
       </div>
@@ -29,7 +28,6 @@ include "universal/view-header.php";
     <div class="card">
       <h5 class="card-header">Points</h5>
       <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
         <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
         <a href="points/points.php" class="btn btn-primary">Points</a>
       </div>
@@ -39,7 +37,6 @@ include "universal/view-header.php";
     <div class="card">
       <h5 class="card-header">Brothers - Charts</h5>
       <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
         <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
         <a href="charts/charts.php" class="btn btn-primary">Charts</a>
       </div>
@@ -47,7 +44,6 @@ include "universal/view-header.php";
     <div class="card">
       <h5 class="card-header">Attendance</h5>
       <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
         <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
         <a href="attendance/attendance.php" class="btn btn-primary">Attendance</a>
       </div>
